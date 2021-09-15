6c98a0798e82ab6cbb853d7c8de8335ff0a5f372
Sourav Das <souravdas78745@gmail.com>
2021-09-16 00:37:59 +0530

    created tablayout and viewpager2 with fragments.

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -3,15 +3,29 @@ package com.example.chatapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.chatapp.Fragments.ChatsFragment;
+import com.example.chatapp.Fragments.UsersFragment;
 import com.example.chatapp.Model.User;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +33,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -66,10 +82,20 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        TabLayout tabLayout =findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(this);
 
-
+        ArrayList<String> titles1 = new ArrayList<>();
+        titles1.add("Chats");
+        titles1.add("Users");
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        viewPager.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(tabLayout,viewPager,((tab, position) -> tab.setText(titles1.get(position)))).attach();
     }
 
+    //menu inflation
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
@@ -86,4 +112,42 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
+
+
+
+
+
+    //fragments
+    class ViewPagerAdapter extends FragmentStateAdapter  {
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+            this.fragments=new ArrayList<>();
+            this.titles=new ArrayList<>();
+        }
+
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment,String title)
+        {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+    }
+
+
 }
